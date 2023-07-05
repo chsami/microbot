651f4d38a11e98606cb2ac2fb08f2be529c2cdc7
Nathan <nathanadsmith1@gmail.com>
2023-07-05 17:08:50 +0100

    add Nates Scripts

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/nategoldrings/GoldConfig.java
@@ -0,0 +1,10 @@
+package net.runelite.client.plugins.nategoldrings;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+
+@ConfigGroup("GoldRings")
+public interface GoldConfig extends Config {
+
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/nateminer/MiningOverlay.java
@@ -0,0 +1,80 @@
+package net.runelite.client.plugins.nateminer;
+
+import net.runelite.api.Skill;
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.ui.overlay.OverlayPanel;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.components.LineComponent;
+import net.runelite.client.ui.overlay.components.TitleComponent;
+
+import javax.inject.Inject;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+
+import static net.runelite.client.plugins.nateminer.Info.*;
+
+public class MiningOverlay extends OverlayPanel {
+
+    private String ft(long duration)
+    {
+        String res = "";
+        long days = TimeUnit.MILLISECONDS.toDays(duration);
+        long hours = TimeUnit.MILLISECONDS.toHours(duration)
+                - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(duration));
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(duration)
+                - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS
+                .toHours(duration));
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(duration)
+                - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS
+                .toMinutes(duration));
+        if (days == 0) {
+            res = (hours + ":" + minutes + ":" + seconds);
+        } else {
+            res = (days + ":" + hours + ":" + minutes + ":" + seconds);
+        }
+        return res;
+    }
+    @Inject
+    MiningOverlay(MiningPlugin plugin)
+    {
+        super(plugin);
+        setPosition(OverlayPosition.TOP_LEFT);
+    }
+    @Override
+    public Dimension render(Graphics2D graphics) {
+        try {
+            xpGained = Microbot.getClient().getSkillExperience(Skill.MINING) - expstarted;
+            int xpPerHour = (int)( xpGained / ((System.currentTimeMillis() - timeBegan) / 3600000.0D));
+            nextLevelXp = XP_TABLE[Microbot.getClient().getRealSkillLevel(Skill.MINING) + 1];
+            xpTillNextLevel = nextLevelXp - Microbot.getClient().getSkillExperience(Skill.MINING);
+            if (xpGained >= 1)
+            {
+                timeTNL = (long) ((xpTillNextLevel / xpPerHour) * 3600000);
+            }
+            panelComponent.setPreferredSize(new Dimension(275, 700));
+            panelComponent.getChildren().add(TitleComponent.builder()
+                    .text("Nate's Power Miner")
+                    .color(Color.darkGray)
+                    .build());
+
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left("Mining Exp Gained (hr): " + (xpGained)  + " ("+xpPerHour+")")
+                    .build());
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left("Mining Levels Gained: " + ( Microbot.getClient().getRealSkillLevel(Skill.MINING) - startinglevel))
+                    .build());
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left("Time till next level: " + ft(timeTNL))
+                    .build());
+
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left(Microbot.status)
+                    .build());
+
+
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return super.render(graphics);
+    }
+}
