abe7e9108473d9f4cebe57f0958f7a03b20292aa
Nathan <nathanadsmith1@gmail.com>
2023-07-20 15:01:48 +0100

    updated plugins to be in one folder

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/nateplugins/nategoldrings/nategoldrings/GoldConfig.java
@@ -0,0 +1,10 @@
+package net.runelite.client.plugins.nateplugins.nategoldrings.nategoldrings;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+
+@ConfigGroup("GoldRings")
+public interface GoldConfig extends Config {
+
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/nateplugins/natepieshells/natepieshells/PieConfig.java
@@ -0,0 +1,10 @@
+package net.runelite.client.plugins.nateplugins.natepieshells.natepieshells;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+
+@ConfigGroup("PieMaking")
+public interface PieConfig extends Config {
+
+}
