22c9809da79bf9c40da59c642d603c873b34c42b
Jancarlos Rodriguez <jrod7938@gmail.com>
2023-08-11 11:26:13 -0400

    corrected config file so plugin can run

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/autoVorkath/AutoVorkathConfig.java
@@ -0,0 +1,39 @@
+package net.runelite.client.plugins.autoVorkath;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("AutoVorkath")
+public interface AutoVorkathConfig extends Config {
+
+    @ConfigItem(
+            keyName = "crossbow",
+            name = "Crossbow",
+            description = "Choose your crossbow",
+            position = 0
+    )
+    default CROSSBOW CROSSBOW() {
+        return CROSSBOW.ARMADYL_CROSSBOW;
+    }
+
+    @ConfigItem(
+            keyName = "slayersStaff",
+            name = "Slayers Staff",
+            description = "Choose your slayers staff",
+            position = 1
+    )
+    default STAFF SLAYERSTAFF() {
+        return STAFF.SLAYER_STAFF;
+    }
+
+    @ConfigItem(
+            keyName = "teleport",
+            name = "Teleport",
+            description = "Choose your teleport",
+            position = 2
+    )
+    default TELEPORT TELEPORT() {
+        return TELEPORT.CONSTRUCT_CAPE_T;
+    }
+}
