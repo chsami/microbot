2c61ddd318674d65ef1aa030d1408c81746c14ac
Jancarlos Rodriguez <jrod7938@gmail.com>
2023-08-11 13:13:16 -0400

    corrected construct cape(t) name

--- runelite-client/src/main/java/net/runelite/client/plugins/autoVorkath/TELEPORT.kt
+++ runelite-client/src/main/java/net/runelite/client/plugins/autoVorkath/TELEPORT.kt
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor
 @Getter
 @RequiredArgsConstructor
 enum class TELEPORT(val teleportName: String) {
-    CONSTRUCT_CAPE_T("Construct. Cape (t)"),
+    CONSTRUCT_CAPE_T("Construct. Cape(t)"),
     CONSTRUCT_CAPE("Construct. Cape"),
     HOUSE_TAB("Teleport to house");
 
