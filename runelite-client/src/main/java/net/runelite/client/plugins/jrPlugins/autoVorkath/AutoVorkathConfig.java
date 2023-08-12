e199f42f1b80e96a57fdd597a2b2892d91371cb7
Jancarlos Rodriguez <jrod7938@gmail.com>
2023-08-12 00:33:40 -0400

    consolidated plugin into jrPlugins folder

--- runelite-client/src/main/java/net/runelite/client/plugins/jrPlugins/autoVorkath/AutoVorkathConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/jrPlugins/autoVorkath/AutoVorkathConfig.java
@@ -1,4 +1,4 @@
-package net.runelite.client.plugins.autoVorkath;
+package net.runelite.client.plugins.jrPlugins.autoVorkath;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
--- runelite-client/src/main/java/net/runelite/client/plugins/jrPlugins/autoVorkath/AutoVorkathPlugin.kt
+++ runelite-client/src/main/java/net/runelite/client/plugins/jrPlugins/autoVorkath/AutoVorkathPlugin.kt
@@ -1,4 +1,4 @@
-package net.runelite.client.plugins.autoVorkath
+package net.runelite.client.plugins.jrPlugins.autoVorkath
 
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
