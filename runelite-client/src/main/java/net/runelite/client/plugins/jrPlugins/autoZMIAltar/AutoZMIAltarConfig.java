c96a758ebec54207b8c25314f7ed5d415fdec4ab
Jancarlos Rodriguez <jrod7938@gmail.com>
2023-09-02 22:19:56 -0400

    updated config

--- runelite-client/src/main/java/net/runelite/client/plugins/jrPlugins/autoZMIAltar/AutoZMIAltarConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/jrPlugins/autoZMIAltar/AutoZMIAltarConfig.java
@@ -24,6 +24,7 @@ public interface AutoZMIAltarConfig extends Config {
     default String GUIDE() {
         return "MUST BE ON LUNAR SPELL BOOK\n" +
                 "MUST HAVE NPC CONTACT RUNES IN INVENTORY\n" +
+                "MUST HAVE JEWELLERY BOX SET TO EDGEVILLE" +
                 "1. Setup Your Inventory Setup with it's name\n" +
                 "2. Start at Edgeville bank";
     }
