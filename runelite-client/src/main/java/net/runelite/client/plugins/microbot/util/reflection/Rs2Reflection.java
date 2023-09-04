5c58070f7206f971f7bafb6da95e1c1c024ab714
chsami <sami.chkhachkhi@gmail.com>
2023-09-04 21:03:48 +0200

    Grounditem supports all menu actions

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/reflection/Rs2Reflection.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/reflection/Rs2Reflection.java
@@ -1,9 +1,13 @@
 package net.runelite.client.plugins.microbot.util.reflection;
 
 import lombok.SneakyThrows;
+import net.runelite.api.ItemComposition;
 import net.runelite.api.NPC;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Rs2Reflection {
     @SneakyThrows
@@ -13,6 +17,21 @@ public class Rs2Reflection {
         int anim = animation.getInt(npc) * -1553687919;
         animation.setAccessible(false);
         return anim;
+    }
+
+    @SneakyThrows
+    public static String[] getGroundItemActions(ItemComposition item) {
+        List<Field> fields = Arrays.stream(item.getClass().getFields()).filter(x -> x.getType().isArray()).collect(Collectors.toList());
+        for (Field field: fields) {
+            if (field.getType().getComponentType().getName() == "java.lang.String") {
+                String[] actions = (String[]) field.get(item);
+                if (Arrays.stream(actions).anyMatch(x -> x!= null && x.equalsIgnoreCase("take"))) {
+                    field.setAccessible(true);
+                    return actions;
+                }
+            }
+        }
+        return new String[] {};
     }
 
 }
