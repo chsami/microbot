4d10e2428a86c41d0909cc909ee19cb15e69e3b9
chsami <sami.chkhachkhi@gmail.com>
2023-09-29 00:03:18 +0200

    fix seed level requirements

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/tithefarm/enums/TitheFarmMaterial.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/tithefarm/enums/TitheFarmMaterial.java
@@ -11,8 +11,8 @@ import net.runelite.client.plugins.microbot.util.inventory.Inventory;
 @RequiredArgsConstructor
 public enum TitheFarmMaterial {
     GOLOVANOVA_SEED("Golovanova seed", 34, '1', ItemID.GOLOVANOVA_FRUIT),
-    BOLOGANO_SEED("Bologano seed", 99, '2', ItemID.BOLOGANO_FRUIT),
-    LOGAVANO_SEED("Logavano seed", 99, '3', ItemID.LOGAVANO_FRUIT);
+    BOLOGANO_SEED("Bologano seed", 54, '2', ItemID.BOLOGANO_FRUIT),
+    LOGAVANO_SEED("Logavano seed", 74, '3', ItemID.LOGAVANO_FRUIT);
 
     final String name;
     final int levelRequired;
