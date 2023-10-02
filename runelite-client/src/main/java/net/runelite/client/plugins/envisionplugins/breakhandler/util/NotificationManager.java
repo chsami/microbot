b721bf7f7b36856cf5d393cc76d637005a1ef003
HyperAlch <Daemonleak@protonmail.com>
2023-10-01 20:04:34 -0400

    Sync with main repo

--- runelite-client/src/main/java/net/runelite/client/plugins/envisionplugins/breakhandler/util/NotificationManager.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/envisionplugins/breakhandler/util/NotificationManager.java
@@ -1,58 +1,44 @@
 package net.runelite.client.plugins.envisionplugins.breakhandler.util;
 
+import net.runelite.client.config.Config;
 import net.runelite.client.plugins.envisionplugins.breakhandler.enums.BreakHandlerStates;
 
 public class NotificationManager {
-    private final DiscordWebhook discordWebhook;
+    private DiscordWebhook discordWebhook;
 
-    private final boolean logToConsole;
-    private final boolean sendDiscordNotifications;
-    private final int messageLimit;
-    private int messageCount;
-    private final int discordNotificationLimit;
-    private int discordNotificationCount;
-    private final String clientName;
+    private boolean logToConsole;
+    private boolean sendDiscordNotifications;
+    private int messageLimit;
+    private int discordNotificationLimit;
 
-
-
-    public NotificationManager(String discordWebhookEndpoint, boolean verboseLogging, boolean discordNotifications, int logMessageLimit, int discordMessageLimit, String name) {
+    public NotificationManager(String discordWebhookEndpoint, boolean verboseLogging, boolean discordNotifications, int logMessageLimit, int discordMessageLimit) {
         discordWebhook = new DiscordWebhook(discordWebhookEndpoint);
 
         logToConsole = verboseLogging;
         sendDiscordNotifications = discordNotifications;
 
         messageLimit = logMessageLimit;
-        messageCount = 0;
         discordNotificationLimit = discordMessageLimit;
-        discordNotificationCount = 0;
-        clientName = name;
     }
 
     public void log(String message) {
-        if (logToConsole && messageCount < messageLimit) {
+        if (logToConsole && messageLimit == 0) {
             System.out.println(message);
-            messageCount++;
-        }
-    }
-
-    public void err(String message) {
-        if (logToConsole && messageCount < messageLimit) {
-            System.err.println(message);
-            messageCount++;
+            messageLimit++;
         }
     }
 
     public void logState(BreakHandlerStates state) {
-        if (logToConsole && messageCount < messageLimit) {
+        if (logToConsole && messageLimit == 0) {
             System.out.println("STATE: " + state);
-            messageCount++;
+            messageLimit++;
         }
     }
 
     public void notifyDiscord(boolean sendDetailedReport, String clientName, String parentPluginName,
                               String[] skillExperienceGained, String[] resourcesGained, String gpGained, String message) {
-        if (sendDiscordNotifications && discordNotificationCount < discordNotificationLimit) {
-            discordNotificationCount++;
+        if (sendDiscordNotifications && discordNotificationLimit == 0) {
+            discordNotificationLimit++;
 
             if (sendDetailedReport) {
                 discordWebhook.sendClientStatusWithGains(
@@ -69,19 +55,11 @@ public class NotificationManager {
         }
     }
 
-    public void simpleNotifyDiscord(String parentPluginName, String message) {
-        if (sendDiscordNotifications && discordNotificationCount < discordNotificationLimit) {
-            discordNotificationCount++;
-
-            discordWebhook.sendClientStatus(clientName, parentPluginName, message);
-        }
-    }
-
     public void resetVerboseMessageCount() {
-        messageCount = 0;
+        messageLimit = 0;
     }
 
     public void resetDiscordNotificationCount() {
-        discordNotificationCount = 0;
+        discordNotificationLimit = 0;
     }
 }
