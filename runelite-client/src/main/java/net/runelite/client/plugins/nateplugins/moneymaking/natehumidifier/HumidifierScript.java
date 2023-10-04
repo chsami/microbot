d29fe622bf3b594094c8d735222b335ffaf376e6
chsami <sami.chkhachkhi@gmail.com>
2023-10-04 23:14:21 +0200

    humidifier script improvements and static walk to camera spinning removed

--- runelite-client/src/main/java/net/runelite/client/plugins/nateplugins/moneymaking/natehumidifier/HumidifierScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/nateplugins/moneymaking/natehumidifier/HumidifierScript.java
@@ -1,5 +1,6 @@
 package net.runelite.client.plugins.nateplugins.moneymaking.natehumidifier;
 
+import net.runelite.api.ItemID;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
 import net.runelite.client.plugins.microbot.util.bank.Rs2Bank;
@@ -18,12 +19,14 @@ public class HumidifierScript extends Script {
             if (!super.run()) return;
 
             try {
+                boolean hasAstralRunesInInventory = Inventory.hasItem(ItemID.ASTRAL_RUNE);
                 if (Microbot.pauseAllScripts) return;
-                if (Inventory.hasItem(config.ITEM().getName())  && (Inventory.hasItemAmountStackable("Astral Rune",0))) {
+                if (Inventory.hasItem(config.ITEM().getName())
+                        && hasAstralRunesInInventory) {
                     Rs2Magic.cast(MagicAction.HUMIDIFY);
                     sleepUntilOnClientThread(() -> Inventory.hasItem(config.ITEM().getFinished()));
                 } else {
-                    bank(config);
+                    bank(config, hasAstralRunesInInventory);
                 }
             } catch (Exception ex) {
                 System.out.println(ex.getMessage());
@@ -32,20 +35,32 @@ public class HumidifierScript extends Script {
         return true;
     }
 
-    private void bank(HumidifierConfig config){
+    private void bank(HumidifierConfig config, boolean hasAstralRunes){
         if(Rs2Bank.isOpen()){
             System.out.println("finished item name: "+ config.ITEM().getFinished());
             Rs2Bank.depositAll(config.ITEM().getFinished());
             sleepUntil(() -> !Inventory.hasItem(config.ITEM().getFinished()));
-            if(Rs2Bank.hasItem(config.ITEM().getName())) {
-                Rs2Bank.withdrawItemsAll(true, config.ITEM().getName());
-                sleepUntilOnClientThread(() -> Inventory.hasItem(config.ITEM().getName()));
-                Rs2Bank.closeBank();
-                sleepUntilOnClientThread(() -> !Rs2Bank.isOpen());
-            } else {
+            if (!hasAstralRunes && !Rs2Bank.hasItem(ItemID.ASTRAL_RUNE)) {
+                Microbot.showMessage("You have no astral runes left");
+                shutdown();
+                return;
+            }
+            if(!Rs2Bank.hasItem(config.ITEM().getName())) {
                 Microbot.showMessage("Ran out of Materials");
                 shutdown();
+                return;
             }
+
+            if (!hasAstralRunes) {
+                Rs2Bank.withdrawItemsAll(true, "astral rune");
+                sleepUntil(() -> Inventory.hasItem(ItemID.ASTRAL_RUNE));
+            }
+
+            Rs2Bank.withdrawItemsAll(true, config.ITEM().getName());
+            sleepUntilOnClientThread(() -> Inventory.hasItem(config.ITEM().getName()));
+            Rs2Bank.closeBank();
+            sleepUntilOnClientThread(() -> !Rs2Bank.isOpen());
+
         } else {
             Rs2Bank.openBank();
         }
