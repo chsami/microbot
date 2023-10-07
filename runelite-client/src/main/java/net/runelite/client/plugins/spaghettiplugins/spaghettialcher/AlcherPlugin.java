d713009d7267f7f8eb9cf197bc0df03860bad37f
turbofist <137625553+turbofist@users.noreply.github.com>
2023-10-08 01:48:53 +0200

    Update AlcherPlugin.java
    Removed break handler import.

--- runelite-client/src/main/java/net/runelite/client/plugins/spaghettiplugins/spaghettialcher/AlcherPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/spaghettiplugins/spaghettialcher/AlcherPlugin.java
@@ -7,7 +7,6 @@ import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import net.runelite.client.plugins.envisionplugins.breakhandler.BreakHandlerScript;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.util.mouse.VirtualMouse;
 import net.runelite.client.ui.overlay.OverlayManager;
