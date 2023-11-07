0db2f8a302c5bcf661c5c733bcaccbf3de738da3
DetectiveCode <fabs1996@live.co.uk>
2023-11-07 17:02:26 +0800

    Adjusted some config names and tags.

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/deserttreasure2/bosses/leviathan/LeviathanConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/deserttreasure2/bosses/leviathan/LeviathanConfig.java
@@ -3,6 +3,6 @@ package net.runelite.client.plugins.microbot.deserttreasure2.bosses.leviathan;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 
-@ConfigGroup("example")
+@ConfigGroup("leviathanConfig")
 public interface LeviathanConfig extends Config {
 }
