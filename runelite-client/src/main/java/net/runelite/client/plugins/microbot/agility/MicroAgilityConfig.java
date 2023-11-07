16512d3e591544a5d8f551cb9ee78f01fd50031e
DetectiveCode <fabs1996@live.co.uk>
2023-11-07 16:40:55 +0800

    Improved the agility script. - Added random pausing which can be turned on or off via the configuration. - Added support for eating food and configuring HP threshold to eat food at. - Script now stops when the HP threshold is above 0 and there's no food in the inventory. - Added support for Pollnivneach rooftop agility course.

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/agility/MicroAgilityConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/agility/MicroAgilityConfig.java
@@ -20,11 +20,59 @@ public interface MicroAgilityConfig extends Config {
             keyName = "Course",
             name = "Course",
             description = "Choose your agility course",
-            position = 0,
+            position = 1,
             section = generalSection
     )
     default AgilityCourseLocation agilityCourse()
     {
         return AgilityCourseLocation.CANIFIS_ROOFTOP_COURSE;
+    }
+
+    @ConfigItem(
+            keyName = "Hitpoints",
+            name = "Hitpoints treshhold",
+            description = "Use food at certain hitpoint treshhold. If there's no food in the inventory, the script stops. Set to 0 in order to disable.",
+            position = 2,
+            section = generalSection
+    )
+    default int hitpoints()
+    {
+        return 20;
+    }
+
+    @ConfigItem(
+            keyName = "BreakTime",
+            name = "Pause Randomly",
+            description = "Pauses randomly before clicking the next obstacle.",
+            position = 3,
+            section = generalSection
+    )
+    default boolean pauseRandomly()
+    {
+        return true;
+    }
+
+    @ConfigItem(
+            keyName = "pauseMinTime",
+            name = "Pause Min Time",
+            description = "Minimum amount of time to randomly pause for.",
+            position = 4,
+            section = generalSection
+    )
+    default int pauseMinTime()
+    {
+        return 10;
+    }
+
+    @ConfigItem(
+            keyName = "pauseMaxTime",
+            name = "Pause Max Time",
+            description = "Maximum amount of time to randomly pause for.",
+            position = 5,
+            section = generalSection
+    )
+    default int pauseMaxTime()
+    {
+        return 5000;
     }
 }
