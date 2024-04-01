06b75cfea0c5d26ea80fff36c20222fab114200b
chsami <sami.chkhachkhi@gmail.com>
2024-04-01 14:17:22 +0200

    FletchingScript
    Added cutting & stringing method

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/fletching/enums/FletchingMode.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/fletching/enums/FletchingMode.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum FletchingMode {
     UNSTRUNG("Cutting", "knife", 1),
     STRUNG("Stringing", "bow string", 14),
+    UNSTRUNG_STRUNG("Cutting & Stringing", "knife", 1),
     PROGRESSIVE("Progressive Logs Cutting", "knife", 1);
 
 
