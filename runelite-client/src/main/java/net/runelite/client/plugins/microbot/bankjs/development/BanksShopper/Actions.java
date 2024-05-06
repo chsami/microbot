a7fc00ad64f57e4070ea83b05dda553cc0c151eb
bisto.js <ryanrunescapes@outlook.com>
2024-05-06 21:39:43 +0100

    BanksShopper init commit

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/bankjs/development/BanksShopper/Actions.java
@@ -0,0 +1,19 @@
+package net.runelite.client.plugins.microbot.bankjs.development.BanksShopper;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Actions {
+    BUY("Buy"),
+    SELL("Sell");
+
+    private final String action;
+
+    @Override
+    public String toString() {
+        return action;
+    }
+}
