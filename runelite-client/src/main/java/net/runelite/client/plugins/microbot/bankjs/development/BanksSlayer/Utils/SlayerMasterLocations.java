c8f77f0412a40504b0b0862f24d44eeb21e34750
bisto.js <ryanrunescapes@outlook.com>
2024-05-16 22:00:35 +0100

    Remove accidentally added folder from Git

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/bankjs/development/BanksSlayer/Utils/SlayerMasterLocations.java
+++
@@ -1,9 +0,0 @@
-package net.runelite.client.plugins.microbot.bankjs.development.BanksSlayer.Utils;
-
-import net.runelite.api.coords.WorldPoint;
-
-public class SlayerMasterLocations {
-    public static WorldPoint Turael = new WorldPoint(2931, 3536, 0);
-
-}
-
