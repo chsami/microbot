02050cbc7fcdb09d54b6776155bd0627488babf5
chsami <sami.chkhachkhi@gmail.com>
2024-06-13 08:29:51 +0200

    Removal lucid plugins & ethan api

--- runelite-client/src/main/java/net/runelite/client/plugins/hoseaplugins/lucidtobprayers/EventType.java
+++
@@ -1,7 +0,0 @@
-package net.runelite.client.plugins.hoseaplugins.lucidtobprayers;
-
-public enum EventType
-{
-    ANIMATION_CHANGED, NPC_SPAWNED, NPC_DESPAWNED, NPC_CHANGED, PROJECTILE_SPAWNED,
-    GRAPHICS_CREATED, GAME_OBJECT_SPAWNED, OTHER_INTERACT_YOU, YOU_INTERACT_OTHER, ITEM_EQUIPPED
-}
