3c991d019dbdddef0d095b81d6234ce0cb606785
Bank.js <ryanrunescapes@outlook.com>
2024-06-14 22:37:07 +0100

    Removed Dev Folder

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/bankjs/development/BanksSlayer/enums/State.java
+++
@@ -1,9 +0,0 @@
-package net.runelite.client.plugins.microbot.bankjs.development.BanksSlayer.enums;
-
-public enum State {
-    GET_TASK,
-    RESTOCK,
-    TRAVEL_TO_MONSTER,
-    FIGHT_MONSTER,
-    IDLE
-}
