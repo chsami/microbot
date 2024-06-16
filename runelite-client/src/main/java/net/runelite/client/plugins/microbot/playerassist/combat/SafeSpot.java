d1ce3d98b117f7e3df8046f8afdfd650c2e5f500
SexyPinata <61428716+SexyPinata@users.noreply.github.com>
2024-06-16 19:01:13 +0200

    AIO Fighter: Will now retarget same NPC after safe spot

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/playerassist/combat/SafeSpot.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/playerassist/combat/SafeSpot.java
@@ -1,12 +1,19 @@
 package net.runelite.client.plugins.microbot.playerassist.combat;
 
+import net.runelite.api.NPC;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
 import net.runelite.client.plugins.microbot.playerassist.PlayerAssistConfig;
+import net.runelite.client.plugins.microbot.playerassist.PlayerAssistPlugin;
+import net.runelite.client.plugins.microbot.util.npc.Rs2Npc;
 import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class SafeSpot extends Script {
 
@@ -14,6 +21,7 @@ public class SafeSpot extends Script {
     private boolean messageShown = false;
 
 public boolean run(PlayerAssistConfig config) {
+    AtomicReference<List<String>> npcsToAttack = new AtomicReference<>(Arrays.stream(Arrays.stream(config.attackableNpcs().split(",")).map(String::trim).toArray(String[]::new)).collect(Collectors.toList()));
     mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
         try {
             if (!Microbot.isLoggedIn() || !super.run() || !config.toggleSafeSpot() || Microbot.isMoving()) return;
@@ -27,13 +35,49 @@ public boolean run(PlayerAssistConfig config) {
                 }
                 return;
             }
-            if (isDefaultSafeSpot(currentSafeSpot) || isPlayerAtSafeSpot(currentSafeSpot)) return;
+            if (isDefaultSafeSpot(currentSafeSpot) || isPlayerAtSafeSpot(currentSafeSpot)) {
+                //ckeck if there is an NPC targeting us
+                List<NPC> npcList = Rs2Npc.getNpcsAttackingPlayer(Microbot.getClient().getLocalPlayer());
+
+                //if there is an NPC interacting with us, and we are not Interacting with it, attack it again
+                if (!npcList.isEmpty()) {
+                    npcList.forEach(npc -> {
+                        if (Microbot.getClient().getLocalPlayer().getInteracting() == null) {
+                            if (npcsToAttack.get().contains(npc.getName())) {
+                                Rs2Npc.attack(npc);
+                                PlayerAssistPlugin.setCooldown(config.playStyle().getRandomTickInterval());
+                            }
+                        }
+                    });
+                    return;
+                }
+
+                return;
+            }
+
             messageShown = false;
 
             if(Rs2Walker.walkMiniMap(currentSafeSpot)) {
                 Microbot.pauseAllScripts = true;
                 sleepUntil(() -> isPlayerAtSafeSpot(currentSafeSpot));
                 Microbot.pauseAllScripts = false;
+                //ckeck if there is an NPC targeting us
+                List<NPC> npcList = Rs2Npc.getNpcsAttackingPlayer(Microbot.getClient().getLocalPlayer());
+
+                //if there is an NPC interacting with us, and we are not Interacting with it, attack it again
+                if (!npcList.isEmpty()) {
+                    npcList.forEach(npc -> {
+                        if (Microbot.getClient().getLocalPlayer().getInteracting() == null) {
+                            if (npcsToAttack.get().contains(npc.getName())) {
+                                Rs2Npc.attack(npc);
+                                PlayerAssistPlugin.setCooldown(config.playStyle().getRandomTickInterval());
+                            }
+
+                        }
+                    });
+                }
+
+
             }
 
         } catch (Exception ex) {
