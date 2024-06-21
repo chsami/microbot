7bb6280589014c789f4abd0133d4bd0872313a3b
SexyPinata <61428716+SexyPinata@users.noreply.github.com>
2024-06-21 15:23:09 +0200

    MLM Rework: NEW: Upper level added NEW: Upgraded sack is automatically supported CHANGE: Improved Ore vein handling CHANGE: Fix wheel if both are broken CHANGE: Improved sack and bank handling

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/mining/motherloadmine/enums/MLMMiningSpot.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/mining/motherloadmine/enums/MLMMiningSpot.java
@@ -13,9 +13,10 @@ public enum MLMMiningSpot {
     IDLE(null),
     WEST_LOWER(Arrays.asList(new WorldPoint(3731, 5661, 0), new WorldPoint(3730, 5668, 0))),
     WEST_MID(null),
-    WEST_UPPER(null),
+    WEST_UPPER(Arrays.asList(new WorldPoint(3752, 5683, 0), new WorldPoint(3752, 5680, 0))),
     SOUTH(Arrays.asList(new WorldPoint(3745, 5647, 0), new WorldPoint(3756, 5653, 0))),
-    NORTH_UPPER(null);
+    NORTH(null);
+    //EAST_UPPER(Arrays.asList(new WorldPoint(3760, 5671, 0), new WorldPoint(3758, 5673, 0)));
 
     private final List<WorldPoint> worldPoint;
 
