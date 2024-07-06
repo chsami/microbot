1809c86e0f8c497b08ad80f043a86dbad05a5681
chsami <sami.chkhachkhi@gmail.com>
2024-07-06 18:52:32 +0200

    fix: thieving script do not drop coins if your inventory is full

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/thieving/ThievingScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/thieving/ThievingScript.java
@@ -161,6 +161,7 @@ public class ThievingScript extends Script {
 
         doNotDropItemList.add(config.food().getName());
         doNotDropItemList.add("dodgy necklace");
+        doNotDropItemList.add("coins");
         Rs2Inventory.dropAllExcept(config.keepItemsAboveValue(), doNotDropItemList);
     }
 }
