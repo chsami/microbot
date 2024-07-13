b7fa6cb7c9fa195df020a740ba5e18e57f1d59f8
rastinder <rastinder@gmail.com>
2024-07-13 17:07:16 +0530

    Update Rs2Inventory.java
    added ItemQuantity function which returns total quantity of a item

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/inventory/Rs2Inventory.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/inventory/Rs2Inventory.java
@@ -849,6 +849,44 @@ public class Rs2Inventory {
         if (item == null) return false;
         return hasItemAmount(name, amount, item.isStackable(), false);
     }
+    
+    /**
+     * Retrieves the quantity of an item based on its ID.
+     *
+     * @param id The ID of the item.
+     * @return The quantity of the item if found, otherwise 0.
+     */
+    public static long ItemQuantity(int id) {
+        Rs2Item rs2Item = get(id);
+        if (rs2Item != null) {
+            if (rs2Item.isStackable()) {
+                return rs2Item.quantity;
+            } else {
+                return items().stream().filter(x -> x.id == id).count();
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Retrieves the quantity of an item based on its name.
+     *
+     * @param itemName The name of the item.
+     * @return The quantity of the item if found, otherwise 0.
+     */
+    public static long ItemQuantity(String itemName) {
+        Rs2Item rs2Item = get(itemName);
+        if (rs2Item != null) {
+            if (rs2Item.isStackable()) {
+                return rs2Item.quantity;
+            } else {
+                return items().stream().filter(x -> x.id == rs2Item.getId()).count();
+            }
+        } else {
+            return 0;
+        }
+    }
 
     /**
      * Checks if the player has a certain quantity of an item.
