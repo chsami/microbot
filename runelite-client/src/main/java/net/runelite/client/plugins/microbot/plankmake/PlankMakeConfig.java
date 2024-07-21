3bc8c635516a9883164672887bd13de97e37f861
yace0 <109323993+yace0@users.noreply.github.com>
2024-07-21 01:11:10 +0100

    1.0.1

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/plankmake/PlankMakeConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/plankmake/PlankMakeConfig.java
@@ -8,37 +8,59 @@ import net.runelite.client.plugins.microbot.plankmake.enums.Logs;
 
 @ConfigGroup("plankMake")
 public interface PlankMakeConfig extends Config {
-        String GROUP = "Plank Make";
-
-        @ConfigItem(
-                keyName = "guide",
-                name = "How to use",
-                description = "How to use this plugin",
-                position = 0,
-                section = generalSection
-        )
-        default String GUIDE() {
-            return "Start at any bank with runes and coins in your inventory and make sure you have\n" +
-                    "already pre-cast plank make on the log you want to use and click don't ask again \n" +
-                    "on the pop up and everything should be good to go.";
-        }
-
-        @ConfigSection(
-                name = "General",
-                description = "General",
-                position = 0,
-                closedByDefault = false
-        )
-        String generalSection = "general";
-
-
+    String GROUP = "Plank Make";
 
     @ConfigItem(
-        keyName = "logType",
-        name = "Log Type",
-        description = "Type of plank to make"
+            keyName = "guide",
+            name = "How to use",
+            description = "How to use this plugin",
+            position = 0,
+            section = generalSection
+    )
+    default String GUIDE() {
+        return "Start at any bank with runes and coins in your inventory and make sure you have\n" +
+                "already pre-cast plank make on the log you want to use and click don't ask again \n" +
+                "on the pop up and everything should be good to go.";
+    }
+
+    @ConfigSection(
+            name = "General",
+            description = "General",
+            position = 0,
+            closedByDefault = false
+    )
+    String generalSection = "general";
+
+    @ConfigItem(
+            keyName = "logType",
+            name = "Log Type",
+            description = "Type of plank to make",
+            position = 1,
+            section = generalSection
     )
     default Logs ITEM() {
         return Logs.LOGS;
+    }
+
+    @ConfigItem(
+            keyName = "lazyMode",
+            name = "Enable Lazy Mode",
+            description = "Enable lazy mode to add random delays and make the script less tick perfect",
+            position = 2,
+            section = generalSection
+    )
+    default boolean lazyMode() {
+        return false;
+    }
+
+    @ConfigItem(
+            keyName = "maxLazyDelay",
+            name = "Maximum Lazy Delay",
+            description = "The maximum additional delay in milliseconds to add in lazy mode",
+            position = 3,
+            section = generalSection
+    )
+    default int maxLazyDelay() {
+        return 500; // Default to 500 milliseconds
     }
 }
