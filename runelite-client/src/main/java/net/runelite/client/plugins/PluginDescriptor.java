983fc400190bffb12c0aff2733a8edbdb66f39c7
Corbin <corbinkilmer@gmail.com>
2024-08-04 20:31:54 -0700

    Add Basm plugin descriptor

--- runelite-client/src/main/java/net/runelite/client/plugins/PluginDescriptor.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/PluginDescriptor.java
@@ -43,6 +43,7 @@ public @interface PluginDescriptor {
     String Basche = "<html>[<font color=#07A6F0>B</font>] ";
     String Vince = "<html>[<font color=#5bffe4>V</font>] ";
     String Geoff = "<html>[<font color=#ffbc03>\uD83C\uDF41G</font>] ";
+    String Basm = "<html>[<font color=#b3b3b3>W</font>] ";
 
     String name();
 
