840b90f6aaaf82f712ca8a1b386adeb4ec2de01d
SexyPinata <61428716+SexyPinata@users.noreply.github.com>
2024-08-22 08:01:27 +0200

    QoL plugin, render maxhit over monsters

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/npc/Rs2NpcManager.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/npc/Rs2NpcManager.java
@@ -1,7 +1,9 @@
 package net.runelite.client.plugins.microbot.util.npc;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
@@ -17,22 +19,24 @@ import java.util.Set;
  * This class is responsible for managing NPCs in the game.
  * It provides utility methods for loading NPC data from JSON files and retrieving NPC stats.
  */
+@Slf4j
 public class Rs2NpcManager {
     private static final Set<Integer> blacklistXpMultiplier = Set.of(8026, 8058, 8059, 8060, 8061, 7850, 7852, 7853, 7884, 7885, 7849, 7851, 7854, 7855, 7882, 7883, 7886, 7887, 7888, 7889, 494, 6640, 6656, 2042, 2043, 2044);
-    private static Map<Integer, Rs2NpcStats> statsMap;
     public static Map<Integer, String> attackStyleMap;
     public static Map<Integer, String> attackAnimationMap;
+    private static Map<Integer, Rs2NpcStats> statsMap;
 
     /**
      * Loads NPC data from JSON files.
      * This method should be called before using any other methods in this class.
      */
-    public static void loadJson() {
+    public static void loadJson() throws Exception {
         if (statsMap != null) {
             return;
         }
         Type statsTypeToken = new TypeToken<Map<Integer, Rs2NpcStats>>() {}.getType();
-        statsMap = loadJsonFile("/npc/npc_stats.json", statsTypeToken);
+
+        statsMap = loadNpcStatsFromJsonFile("/npc/monsters_complete.json");
 
         Type attackStyleTypeToken = new TypeToken<Map<Integer, String>>() {}.getType();
         attackStyleMap = loadJsonFile("/npc/npcs_attack_style.json", attackStyleTypeToken);
@@ -61,6 +65,30 @@ public class Rs2NpcManager {
         }
     }
 
+    private static Map<Integer, Rs2NpcStats> loadNpcStatsFromJsonFile(String filename) {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Rs2NpcStats.class, Rs2NpcStats.NPC_STATS_TYPE_ADAPTER)
+                .create();
+
+        try (InputStream inputStream = Rs2NpcStats.class.getResourceAsStream(filename)) {
+            if (inputStream == null) {
+                System.out.println("Failed to load " + filename);
+                return Collections.emptyMap();
+            }
+
+            // Deserialize the JSON directly into a Map<Integer, Rs2NpcStats>
+            Type typeToken = new TypeToken<Map<Integer, Rs2NpcStats>>() {
+            }.getType();
+            Map<Integer, Rs2NpcStats> statsMap = gson.fromJson(new InputStreamReader(inputStream, StandardCharsets.UTF_8), typeToken);
+
+            return statsMap;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading JSON file: " + filename, e);
+        }
+    }
+
+
     /**
      * Retrieves the stats of an NPC.
      * @param npcId The ID of the NPC.
@@ -88,6 +116,7 @@ public class Rs2NpcManager {
      */
     public static int getAttackSpeed(int npcId) {
         Rs2NpcStats s = statsMap.get(npcId);
+        log.info(s.toString());
         return s != null && s.getAttackSpeed() != -1 ? s.getAttackSpeed() : -1;
     }
 
