bc591ae0202b3ae44b9cc9d7860fd6756b63241f
chsami <sami.chkhachkhi@gmail.com>
2024-09-03 23:31:42 +0200

    Allow information box to be set for plugin configs

---
+++ runelite-client/src/main/java/net/runelite/client/config/ConfigInformation.java
@@ -0,0 +1,13 @@
+package net.runelite.client.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ConfigInformation
+{
+    String value();
+}
