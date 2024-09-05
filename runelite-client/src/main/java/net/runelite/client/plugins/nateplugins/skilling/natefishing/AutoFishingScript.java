4b536ac52bb7616e1168361467fbf21c648c9974
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2024-09-05 01:17:08 -0400

    feat: implement antiban features feat: add cave eels

--- runelite-client/src/main/java/net/runelite/client/plugins/nateplugins/skilling/natefishing/AutoFishingScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/nateplugins/skilling/natefishing/AutoFishingScript.java
@@ -3,6 +3,8 @@ package net.runelite.client.plugins.nateplugins.skilling.natefishing;
 import net.runelite.api.NPC;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
+import net.runelite.client.plugins.microbot.util.antiban.Rs2Antiban;
+import net.runelite.client.plugins.microbot.util.antiban.Rs2AntibanSettings;
 import net.runelite.client.plugins.microbot.util.bank.Rs2Bank;
 import net.runelite.client.plugins.microbot.util.camera.Rs2Camera;
 import net.runelite.client.plugins.microbot.util.inventory.Rs2Inventory;
@@ -15,7 +17,6 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import static net.runelite.client.plugins.microbot.util.Global.sleepUntilTrue;
 import static net.runelite.client.plugins.microbot.util.npc.Rs2Npc.validateInteractable;
 
 enum State {
@@ -33,11 +34,13 @@ public class AutoFishingScript extends Script {
         List<String> itemNames = Arrays.stream(config.itemsToBank().split(",")).map(String::toLowerCase).collect(Collectors.toList());
         state = State.FISHING;
         Fish fish = config.fish();
+        Rs2Antiban.resetAntibanSettings();
+        Rs2Antiban.antibanSetupTemplates.applyFishingSetup();
         mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
             try {
-
                 if (!super.run()) return;
                 if (!Microbot.isLoggedIn()) return;
+                if (Rs2AntibanSettings.actionCooldownActive) return;
 
                 if (initialPlayerLocation == null) {
                     initialPlayerLocation = Rs2Player.getWorldLocation();
@@ -63,18 +66,19 @@ public class AutoFishingScript extends Script {
                             validateInteractable(fishingSpot);
                         }
                         if (Rs2Npc.interact(fishingSpot, fish.getAction())) {
-                            sleepUntilTrue(() -> !Rs2Player.isAnimating(), 500, 30000);
+                            Rs2Antiban.actionCooldown();
+                            Rs2Antiban.takeMicroBreakByChance();
+                            Rs2Player.waitForAnimation(30000);
                         }
                         break;
                     case RESETTING:
                         if (config.useBank()) {
                             if (!Rs2Bank.bankItemsAndWalkBackToOriginalPosition(itemNames, initialPlayerLocation))
                                 return;
-                            state = State.FISHING;
                         } else {
-                            Rs2Inventory.dropAllExcept(false, config.getDropOrder(), "rod", "net", "pot", "harpoon", "feather", "bait", "vessel");
-                            state = State.FISHING;
+                            Rs2Inventory.dropAllExcept(false, config.getDropOrder(), "rod", "net", "pot", "harpoon", "feather", "bait", "vessel", "candle", "lantern");
                         }
+                        state = State.FISHING;
                         break;
                 }
             } catch (Exception ex) {
@@ -99,6 +103,8 @@ public class AutoFishingScript extends Script {
                 return Rs2Inventory.hasItem("lobster pot");
             case LAVA_EEL:
                 return Rs2Inventory.hasItem("oily fishing rod") && Rs2Inventory.hasItem("fishing bait");
+            case CAVE_EEL:
+                return Rs2Inventory.hasItem("fishing rod") && Rs2Inventory.hasItem("fishing bait");
             default:
                 return false;
         }
