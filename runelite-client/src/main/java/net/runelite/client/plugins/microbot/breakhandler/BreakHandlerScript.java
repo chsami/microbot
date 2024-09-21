4943e9c9115dae8e7617ecbae1d7157515f82dce
SexyPinata <61428716+SexyPinata@users.noreply.github.com>
2024-09-21 13:55:36 +0200

    # **Break Handler** #
    - **Fixed**: Only make a login atempt if player is logged out.

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/breakhandler/BreakHandlerScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/breakhandler/BreakHandlerScript.java
@@ -39,6 +39,7 @@ public class BreakHandlerScript extends Script {
         long seconds = duration.getSeconds() % 60;
         return String.format(header + " %02d:%02d:%02d", hours, minutes, seconds);
     }
+
     public boolean run(BreakHandlerConfig config) {
 
         Microbot.enableAutoRunOn = false;
@@ -80,7 +81,8 @@ public class BreakHandlerScript extends Script {
                     Microbot.pauseAllScripts = false;
                     if (breakIn <= 0)
                         breakIn = Random.random(config.timeUntilBreakStart() * 60, config.timeUntilBreakEnd() * 60);
-                    new Login();
+                    if (!Microbot.isLoggedIn())
+                        new Login();
                     totalBreaks++;
                     ClientUI.getFrame().setTitle(title);
                     if (Rs2AntibanSettings.takeMicroBreaks) {
