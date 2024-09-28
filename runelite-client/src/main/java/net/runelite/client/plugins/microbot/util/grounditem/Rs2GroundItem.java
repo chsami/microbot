56fff330a709e39d21b66823f184fa26931281fd
SexyPinata <61428716+SexyPinata@users.noreply.github.com>
2024-09-28 13:38:18 +0200

    ## **Class: [Rs2GroundItem]** - **Major Fix**: Looting nolonger getting stuck in `pauseAllScripts` mode. - **Minor Fix**: `LootingParamaters` for looting by value now has a default minimum Quantity change from 0 to 1. - **Added**: ` waitForGroundItemDespawn(Runnable actionWhileWaiting,GroundItem groundItem) ` method to wait for a ground item to despawn while performing an action. This method is primarily used to try and loot an item until either the item is looted by any player or despawns.

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/grounditem/Rs2GroundItem.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/grounditem/Rs2GroundItem.java
@@ -9,6 +9,7 @@ import net.runelite.client.plugins.grounditems.GroundItem;
 import net.runelite.client.plugins.grounditems.GroundItemsPlugin;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.util.inventory.Rs2Inventory;
+import net.runelite.client.plugins.microbot.util.math.Random;
 import net.runelite.client.plugins.microbot.util.menu.NewMenuEntry;
 import net.runelite.client.plugins.microbot.util.models.RS2Item;
 import net.runelite.client.plugins.microbot.util.player.Rs2Player;
@@ -25,7 +26,6 @@ import java.util.stream.Collectors;
 
 import static net.runelite.api.TileItem.OWNERSHIP_SELF;
 import static net.runelite.client.plugins.microbot.util.Global.sleepUntil;
-import static net.runelite.client.plugins.microbot.util.Global.sleepUntilTrue;
 
 @Slf4j
 public class Rs2GroundItem {
@@ -256,6 +256,26 @@ public class Rs2GroundItem {
         return false;
     }
 
+    /**
+     * Waits for the ground item to despawn while performing an action. (The action should be an interaction with the ground item)
+     *<p> This method proves to be more reliable than {@link Rs2Inventory#waitForInventoryChanges} as it could cause endless loops of trying to loot the same item if the items was looted by another player
+     * or if the player has a Open Herb Sack, Gem Bag or Seed Box etc... and the item was deposited directly into one of those containers bypassing the inventory, resulting in no inventory change.
+     *
+     * <p> This method won't be plagued by the same issues as it monitors the ground item itself for despawn/change.
+     *
+     * @param actionWhileWaiting The action to perform while waiting for the item to despawn
+     * @param groundItem The ground item to monitor for despawn
+     * @return true if the ground item despawns, false otherwise
+     */
+    public static boolean waitForGroundItemDespawn(Runnable actionWhileWaiting,GroundItem groundItem){
+        sleepUntil(() ->  {
+            actionWhileWaiting.run();
+            sleepUntil(() -> groundItem != GroundItemsPlugin.getCollectedGroundItems().get(groundItem.getLocation(), groundItem.getId()), Random.random(600, 2100));
+            return groundItem != GroundItemsPlugin.getCollectedGroundItems().get(groundItem.getLocation(), groundItem.getId());
+        });
+        return groundItem != GroundItemsPlugin.getCollectedGroundItems().get(groundItem.getLocation(), groundItem.getId());
+    }
+
     private static boolean coreLoot(GroundItem groundItem) {
         final int quantity = groundItem.isStackable() ? 1 : groundItem.getQuantity();
         for (int i = 0; i < quantity; i++) {
@@ -272,19 +292,28 @@ public class Rs2GroundItem {
                     return false;
             }
             Microbot.pauseAllScripts = true;
-            Rs2Inventory.waitForInventoryChanges(() -> interact(groundItem));
+            /** switched to waitForGroundItemDespawn instead of waitForInventoryChanges
+             *  as waitForInventoryChanges can cause endless loops of trying to loot the same item
+             *  even after it has been successfully looted by the player or another player.
+             *  Or if the player has a Open Herb Sack, Gem Bag or Seed Box etc it wont trigger an inventory change.
+             */
+
+            waitForGroundItemDespawn(() -> interact(groundItem), groundItem);
+//            Rs2Inventory.waitForInventoryChanges(() -> interact(groundItem));
         }
         return true;
     }
 
     private static boolean validateLoot(Predicate<GroundItem> filter) {
-        boolean hasLootableItems = sleepUntilTrue(() -> hasLootableItems(filter), 600, 5000);
+        boolean hasLootableItems = hasLootableItems(filter);
         //If there are no more lootable items we succesfully looted everything in the filter
         // true to let the script know that we succesfully looted
         if (!hasLootableItems) {
             Microbot.pauseAllScripts = false;
             return true;
         }
+        // This is needed to make sure we dont get stuck in a endless pause if something goes wrong
+        Microbot.pauseAllScripts = false;
         // If we reach this statement, we most likely still have items to loot, and we return false to the script
         // Script above can handle extra logic if the looting failed
         return false;
@@ -298,7 +327,6 @@ public class Rs2GroundItem {
 
         List<GroundItem> groundItems = GroundItemsPlugin.getCollectedGroundItems().values().stream()
                 .filter(filter)
-                .sorted(Comparator.comparingInt(value -> value.getLocation().distanceTo(Rs2Player.getWorldLocation())))
                 .collect(Collectors.toList());
 
         if (groundItems.size() < params.getMinItems()) return false;
@@ -416,7 +444,7 @@ public class Rs2GroundItem {
         return false;
     }
 
-    @Deprecated(since="1.4.6", forRemoval = true)
+    @Deprecated(since = "1.4.6", forRemoval = true)
     public static boolean lootAllItemBasedOnValue(int value, int range) {
         RS2Item[] groundItems = Microbot.getClientThread().runOnClientThread(() ->
                 Rs2GroundItem.getAll(range)
@@ -433,7 +461,7 @@ public class Rs2GroundItem {
         return false;
     }
 
-    @Deprecated(since="1.4.6", forRemoval = true)
+    @Deprecated(since = "1.4.6", forRemoval = true)
     public static boolean loot(int itemId) {
         if (Rs2Inventory.isFull(itemId)) return false;
         RS2Item[] groundItems = Microbot.getClientThread().runOnClientThread(() ->
