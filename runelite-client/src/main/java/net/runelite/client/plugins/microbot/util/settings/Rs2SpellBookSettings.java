cc7fccf7c074e5193ead05db11533d52f1bb2b76
chsami <sami.chkhachkhi@gmail.com>
2024-09-29 00:14:23 +0200

    Rs2SpellBookSettings docs

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/settings/Rs2SpellBookSettings.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/settings/Rs2SpellBookSettings.java
@@ -11,8 +11,18 @@ import static net.runelite.client.plugins.microbot.util.Global.sleepUntil;
 import static net.runelite.client.plugins.microbot.util.Global.sleepUntilTrue;
 import static net.runelite.client.plugins.microbot.util.widget.Rs2Widget.*;
 
+/**
+ * Rs2SpellBookSettings allows to enable/disable spell book filters
+ */
 public class Rs2SpellBookSettings {
-    // Generic toggleSpellFilter with varbit ID
+    /**
+     * toggle spell filter
+     * @param text
+     * @param toggle
+     * @param varbitId
+     * @param closeInterface
+     * @return
+     */
     public static boolean toggleSpellFilter(String text, boolean toggle, int varbitId, boolean closeInterface) {
         int expectedValue = toggle ? 0 : 1;
         if (Microbot.getVarbitValue(varbitId) == expectedValue) {
@@ -46,6 +56,9 @@ public class Rs2SpellBookSettings {
         return Microbot.getVarbitValue(varbitId) == expectedValue;
     }
 
+    /**
+     * close spell book filter
+     */
     private static void closeSpellBookFilter() {
         if ( hasWidgetText("Spell Filters", 218, 0, true)) {
             Rs2Widget.clickWidget("Filters", Optional.of(218), 0, true);
@@ -53,67 +66,141 @@ public class Rs2SpellBookSettings {
         }
     }
 
+    /**
+     * toggle spell filter
+     * @param text
+     * @param toggle
+     * @param varbitId
+     * @return
+     */
     public static boolean toggleSpellFilter(String text, boolean toggle, int varbitId) {
         return toggleSpellFilter(text, toggle, varbitId, true);
     }
 
-    // Toggle methods for each filter returning boolean
+    /**
+     * toggleCombatSpells
+     * @param toggle
+     * @param closeInterface
+     * @return
+     */
     public static boolean toggleCombatSpells(boolean toggle, boolean closeInterface) {
         return toggleSpellFilter("Show Combat spells", toggle, 6605, closeInterface);
     }
 
+    /**
+     * toggleTeleportSpells
+     * @param toggle
+     * @param closeInterface
+     * @return
+     */
     public static boolean toggleTeleportSpells(boolean toggle, boolean closeInterface) {
         return toggleSpellFilter("Show Teleport spells", toggle, 6609, closeInterface);
     }
 
+    /**
+     * @param toggle
+     * @param closeInterface
+     * @return
+     */
     public static boolean toggleUtilitySpells(boolean toggle, boolean closeInterface) {
         return toggleSpellFilter("Show Utility spells", toggle, 6606, closeInterface);
     }
 
+    /**
+     * @param toggle
+     * @param closeInterface
+     * @return
+     */
     public static boolean toggleLackMagicLevel(boolean toggle, boolean closeInterface) {
         return toggleSpellFilter("Show spells you lack the Magic level to cast", toggle, 6607, closeInterface);
     }
 
+    /**
+     * @param toggle
+     * @param closeInterface
+     * @return
+     */
     public static boolean toggleLackRunes(boolean toggle, boolean closeInterface) {
         return toggleSpellFilter("Show spells you lack the runes to cast", toggle, 6608, closeInterface);
     }
 
+    /**
+     * @param toggle
+     * @param closeInterface
+     * @return
+     */
     public static boolean toggleLackRequirements(boolean toggle, boolean closeInterface) {
         return toggleSpellFilter("Show spells you lack the requirements to cast", toggle, 12137, closeInterface);
     }
 
+    /**
+     * @param toggle
+     * @param closeInterface
+     * @return
+     */
     public static boolean toggleIconResizing(boolean toggle, boolean closeInterface) {
         return toggleSpellFilter("Enable icon resizing (outside PvP areas)", toggle, 6548, closeInterface);
     }
 
+    /**
+     * @param toggle
+     * @return
+     */
     public static boolean toggleCombatSpells(boolean toggle) {
         return toggleSpellFilter("Show Combat spells", toggle, 6605);
     }
 
+    /**
+     * @param toggle
+     * @return
+     */
     public static boolean toggleTeleportSpells(boolean toggle) {
         return toggleSpellFilter("Show Teleport spells", toggle, 6609);
     }
 
+    /**
+     * @param toggle
+     * @return
+     */
     public static boolean toggleUtilitySpells(boolean toggle) {
         return toggleSpellFilter("Show Utility spells", toggle, 6606);
     }
 
+    /**
+     * @param toggle
+     * @return
+     */
     public static boolean toggleLackMagicLevel(boolean toggle) {
         return toggleSpellFilter("Show spells you lack the Magic level to cast", toggle, 6607);
     }
 
+    /**
+     * @param toggle
+     * @return
+     */
     public static boolean toggleLackRunes(boolean toggle) {
         return toggleSpellFilter("Show spells you lack the runes to cast", toggle, 6608);
     }
 
+    /**
+     * @param toggle
+     * @return
+     */
     public static boolean toggleLackRequirements(boolean toggle) {
         return toggleSpellFilter("Show spells you lack the requirements to cast", toggle, 12137, true);
     }
 
+    /**
+     * @param toggle
+     * @return
+     */
     public static boolean toggleIconResizing(boolean toggle) {
         return toggleSpellFilter("Enable icon resizing (outside PvP areas)", toggle, 6548, true);
     }
 
+    /**
+     * @return
+     */
     public static boolean setAllFiltersOn() {
         boolean success = toggleCombatSpells(true, false) &&
                 toggleTeleportSpells(true, false) &&
@@ -129,6 +216,9 @@ public class Rs2SpellBookSettings {
         return success;
     }
 
+    /**
+     * @return
+     */
     public static boolean setAllFiltersOff() {
         boolean success = toggleCombatSpells(false, false) &&
                 toggleTeleportSpells(false, false) &&
