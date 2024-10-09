d5a5da9f4497fe144763a9be10fc97b41a991bde
chsami <sami.chkhachkhi@gmail.com>
2024-10-09 22:26:02 +0200

    bump motherload mine 1.6.7

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/mining/motherloadmine/MotherloadMineScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/mining/motherloadmine/MotherloadMineScript.java
@@ -28,7 +28,7 @@ import static net.runelite.client.plugins.microbot.util.math.Random.random;
 
 @Slf4j
 public class MotherloadMineScript extends Script {
-    public static final String version = "1.6.6";
+    public static final String version = "1.6.7";
     private static final WorldArea WEST_UPPER_AREA = new WorldArea(3748, 5676, 7, 9, 0);
     private static final WorldArea EAST_UPPER_AREA = new WorldArea(3755, 5668, 8, 8, 0);
     private static final WorldPoint HOPPER_DEPOSIT = new WorldPoint(3748, 5674, 0);
