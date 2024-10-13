26f60d394c09c27c8cda6d06b56248d74d565abd
chsami <sami.chkhachkhi@gmail.com>
2024-10-13 20:17:16 +0200

    empty example script

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExampleScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExampleScript.java
@@ -1,13 +1,8 @@
 package net.runelite.client.plugins.microbot.example;
 
-import net.runelite.api.coords.WorldPoint;
-import net.runelite.client.plugins.envisionplugins.breakhandler.util.Login;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
 import net.runelite.client.plugins.microbot.shortestpath.ShortestPathPlugin;
-import net.runelite.client.plugins.microbot.util.combat.Rs2Combat;
-import net.runelite.client.plugins.microbot.util.tabs.Rs2Tab;
-import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
 
 import java.util.concurrent.TimeUnit;
 
@@ -31,8 +26,8 @@ public class ExampleScript extends Script {
                  */
 
                 long startTime = System.currentTimeMillis();
-                int world = Login.getRandomWorld(false, null);
-                Microbot.hopToWorld(world);
+
+
                 long endTime = System.currentTimeMillis();
                 long totalTime = endTime - startTime;
                 System.out.println("Total time for loop " + totalTime);
