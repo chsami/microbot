fddf7d4fa29ba61f06d17c3713ad34dc5718878a
lhndl93 <leehindle1993@gmail.com>
2024-10-19 21:08:45 +0100

    bird hunter plugin

--- runelite-client/src/main/java/net/runelite/client/plugins/PluginDescriptor.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/PluginDescriptor.java
@@ -48,7 +48,8 @@ public @interface PluginDescriptor {
     String zuk = "<html>[<font color=#5F9596>Z</font>] ";
     String GZ = "<html>[<font color=#0077B6>\u2728</font>] ";
     String StickToTheScript = "<html>[<font color=#FF4F00>STTS</font>] ";
-    
+    String zerozero ="<html>[<font color=#000000>00</font>] " ;
+
     String name();
 
     /**
