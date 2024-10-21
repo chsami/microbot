eb63273787cd66a5ac7d48d019aad71cb6c2ac4d
lhndl93 <61633800+lhndl93@users.noreply.github.com>
2024-10-21 16:39:38 +0100

    Updated Scurrius
    Added more config options for food and prayer, with min and max to use
    
    Fixed the prayers to be used correctly and disable/enable correctly
    
    fixed dodging of the dangerous tiles

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/scurrius/ScurriusConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/scurrius/ScurriusConfig.java
@@ -1,18 +1,161 @@
 package net.runelite.client.plugins.microbot.scurrius;
 
+import lombok.Getter;
+import net.runelite.api.ItemID;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Range;
 
 @ConfigGroup("Scurrius")
 public interface ScurriusConfig extends Config {
     @ConfigItem(
-            keyName = "Prioritize",
+            keyName = "foodSelection",
+            name = "Select Food",
+            description = "Select the type of food you want to use",
+            position = 0
+    )
+    default FoodSelection foodSelection() {
+        return FoodSelection.SHARK;
+    }
+
+    @ConfigItem(
+            keyName = "foodAmount",
+            name = "Food Amount",
+            description = "Number of food items to withdraw",
+            position = 1
+    )
+    @Range(
+            min = 1,
+            max = 28
+    )
+    default int foodAmount() {
+        return 20;
+    }
+    @ConfigItem(
+            keyName = "minEatPercent",
+            name = "Min Eat Percent",
+            description = "The minimum health percent at which to eat",
+            position = 2
+    )
+    @Range(
+            min = 1,
+            max = 99
+    )
+    default int minEatPercent() {
+        return 30;
+    }
+
+    @ConfigItem(
+            keyName = "maxEatPercent",
+            name = "Max Eat Percent",
+            description = "The maximum health percent at which to eat",
+            position = 3
+    )
+    @Range(
+            min = 2,
+            max = 100
+    )
+    default int maxEatPercent() {
+        return 50;
+    }
+
+    @ConfigItem(
+            keyName = "potionSelection",
+            name = "Select Prayer Potion",
+            description = "Select whether to use Prayer Potion or Super Restore",
+            position = 4
+    )
+    default PotionSelection potionSelection() {
+        return PotionSelection.PRAYERPOTION;
+    }
+
+    @ConfigItem(
+            keyName = "prayerPotionAmount",
+            name = "Prayer Potion Amount",
+            description = "Number of prayer potions to withdraw",
+            position = 5
+    )
+    @Range(
+            min = 1,
+            max = 28
+    )
+    default int prayerPotionAmount() {
+        return 2;
+    }
+
+    // Min Prayer percent to drink potion
+    @ConfigItem(
+            keyName = "minPrayerPercent",
+            name = "Min Prayer Percent",
+            description = "The minimum prayer percent at which to drink a prayer potion",
+            position = 6
+    )
+    @Range(
+            min = 1,
+            max = 99
+    )
+    default int minPrayerPercent() {
+        return 10;
+    }
+
+    // Max Prayer percent to drink potion
+    @ConfigItem(
+            keyName = "maxPrayerPercent",
+            name = "Max Prayer Percent",
+            description = "The maximum prayer percent at which to drink a prayer potion",
+            position = 7
+    )
+    @Range(
+            min = 2,
+            max = 100
+    )
+    default int maxPrayerPercent() {
+        return 30;
+    }
+
+    @ConfigItem(
+            keyName = "Prioritize Giant rats",
             name = "Prioritize",
             description = "Prioritize Giant rats",
-            position = 0
+            position = 8
     )
     default boolean prioritizeRats() {
         return false;
     }
+
+    @Getter
+    enum FoodSelection {
+        TROUT(ItemID.TROUT),
+        SALMON(ItemID.SALMON),
+        KARAMBWAN(ItemID.COOKED_KARAMBWAN),
+        BASS(ItemID.BASS),
+        SWORDFISH(ItemID.SWORDFISH),
+        LOBSTER(ItemID.LOBSTER),
+        MONKFISH(ItemID.MONKFISH),
+        SHARK(ItemID.SHARK),
+        MANTARAY(ItemID.MANTA_RAY),
+        ANGLERFISH(ItemID.ANGLERFISH);
+
+        private final int itemId;
+
+        FoodSelection(int itemId) {
+            this.itemId = itemId;
+        }
+
+    }
+
+    @Getter
+    enum PotionSelection {
+        PRAYERPOTION(ItemID.PRAYER_POTION4),
+        SUPERRESTORE(ItemID.SUPER_RESTORE4);
+
+        private final int itemId;
+
+        PotionSelection(int itemId) {
+            this.itemId = itemId;
+        }
+
+    }
+
 }
