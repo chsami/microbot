15705ce05668d203fbea0046b7f97b2330e6e6ee
chsami <sami.chkhachkhi@gmail.com>
2024-10-24 23:55:19 +0200

    added getOption & clickOption Rs2Dialogue

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/dialogues/Rs2Dialogue.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/dialogues/Rs2Dialogue.java
@@ -1,9 +1,12 @@
 package net.runelite.client.plugins.microbot.util.dialogues;
 
+import net.runelite.api.widgets.Widget;
 import net.runelite.client.plugins.microbot.util.keyboard.Rs2Keyboard;
 import net.runelite.client.plugins.microbot.util.widget.Rs2Widget;
 
 import java.awt.event.KeyEvent;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Rs2Dialogue {
 
@@ -28,5 +31,19 @@ public class Rs2Dialogue {
 
     public static boolean hasSelectAnOption() {
         return Rs2Widget.hasWidget("Select an Option");
+    }
+
+    public static Widget getOption(String option) {
+        Widget parent =  Rs2Widget.getWidget(219, 1);
+        if (parent == null) return null;
+        return Rs2Widget.findWidget(option, Arrays.stream(parent.getDynamicChildren()).collect(Collectors.toList()));
+    }
+
+    public static boolean clickOption(String option) {
+        Widget widgetOption = getOption(option);
+        if (widgetOption != null) {
+            return Rs2Widget.clickWidget(getOption(option));
+        }
+        return false;
     }
 }
