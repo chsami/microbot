7585ea957955c630d57e59f158b534a424f80e19
chsami <sami.chkhachkhi@gmail.com>
2024-10-26 14:32:14 +0200

    undo ExampleScript.java

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExampleScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExampleScript.java
@@ -2,13 +2,13 @@ package net.runelite.client.plugins.microbot.example;
 
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
-import net.runelite.client.plugins.microbot.util.widget.Rs2Widget;
 
 import java.util.concurrent.TimeUnit;
 
 
 public class ExampleScript extends Script {
 
+    public static boolean test = false;
     public boolean run(ExampleConfig config) {
         Microbot.enableAutoRunOn = false;
         mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
@@ -17,9 +17,8 @@ public class ExampleScript extends Script {
                 if (!super.run()) return;
                 long startTime = System.currentTimeMillis();
 
+                test = false;
 
-                sleepUntil(() -> Rs2Widget.getWidget(219, 1) != null);
-                Rs2Widget.sleepUntilHasWidgetText("Al Kharid", 219, 1, false, 5000);
 
                 long endTime = System.currentTimeMillis();
                 long totalTime = endTime - startTime;
