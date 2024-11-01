6f56f6cce03e4662b2cec832dde0e6b8210d756d
lhndl93 <leehindle1993@gmail.com>
2024-11-01 22:43:04 +0000

    Varrock museum cleaner

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/zerozero/varrockcleaner/VarrockCleanerConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/zerozero/varrockcleaner/VarrockCleanerConfig.java
@@ -2,7 +2,7 @@ package net.runelite.client.plugins.microbot.zerozero.varrockcleaner;
 
 import net.runelite.client.config.*;
 import net.runelite.client.plugins.microbot.util.misc.Rs2Food;
-@ConfigInformation("Start this inside the musem near the finds. You must have the Gloves and Boots equipped, with the Trowel, brush and pick in your inventory.")
+@ConfigInformation("Start this inside the museum near the finds. You must have the Gloves and Boots equipped, with the Trowel, brush and pick in your inventory.")
 @ConfigGroup(VarrockCleanerPlugin.CONFIG)
 
 public interface VarrockCleanerConfig extends Config {
