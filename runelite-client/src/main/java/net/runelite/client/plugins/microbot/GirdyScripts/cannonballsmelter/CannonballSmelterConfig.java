4e4be6cb915f970c6cae1c359b2cfedbe10f6784
kirkm <kirkmcrochet@gmail.com>
2024-11-03 03:04:21 -0500

    Config now uses @ConfigInformation, various fixes implemented per review in Script class

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/GirdyScripts/cannonballsmelter/CannonballSmelterConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/GirdyScripts/cannonballsmelter/CannonballSmelterConfig.java
@@ -2,18 +2,13 @@ package net.runelite.client.plugins.microbot.GirdyScripts.cannonballsmelter;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigInformation;
 
 @ConfigGroup("CannonballSmelter")
+@ConfigInformation ("<div style='background-color:black;color:yellow;padding:20px;'>" +
+                    "<center><h2>Instructions</h2>" +
+                    " <p>Start script in Edgeville bank <br /><br />" +
+                    " <b style='color:red;'>MUST</b> have Ammo mould in inventory <br />" +
+                    " and Steel bars in bank</center></div>")
 public interface CannonballSmelterConfig extends Config {
-    @ConfigItem(
-            keyName = "guide",
-            name = "How to use",
-            description = "Instructions on how to use this plugin",
-            position = 0
-    )
-    default String GUIDE() {
-        return "Start at edgeville or port phasmatys bank " +
-                "with ammo mould in inventory and steel bars in bank";
-    }
 }
