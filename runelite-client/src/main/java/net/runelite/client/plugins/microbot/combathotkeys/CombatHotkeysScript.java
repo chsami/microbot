9a2939084f51af316db5d5fe56875397e903a484
Cicire <VirapDev@gmail.com>
2024-11-09 14:23:33 +0100

    Lower interval

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/combathotkeys/CombatHotkeysScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/combathotkeys/CombatHotkeysScript.java
@@ -29,7 +29,7 @@ public class CombatHotkeysScript extends Script {
             } catch (Exception ex) {
                 System.out.println(ex.getMessage());
             }
-        }, 0, 1000, TimeUnit.MILLISECONDS);
+        }, 0, 100, TimeUnit.MILLISECONDS);
         return true;
     }
 
