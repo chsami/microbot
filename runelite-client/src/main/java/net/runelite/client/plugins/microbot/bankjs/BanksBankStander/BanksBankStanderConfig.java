eb3521018a481010e8c0424d750a7cb3dc94c3c9
lVidStorm <@>
2024-11-18 10:05:43 +1100

    added config option to force deposit-all when banking, improvement to logic to detect leftover item overflow.

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/bankjs/BanksBankStander/BanksBankStanderConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/bankjs/BanksBankStander/BanksBankStanderConfig.java
@@ -198,10 +198,20 @@ public interface BanksBankStanderConfig extends Config {
         return true;
     }
     @ConfigItem(
+            keyName = "depositAll",
+            name = "deposit all",
+            description = "force the bank to deposit all items each time.",
+            position = 6,
+            section = toggles
+    )
+    default boolean depositAll() {
+        return true;
+    }
+    @ConfigItem(
             keyName = "withdrawAll",
             name = "withdraw all",
             description = "for using things like a chisel or knife where the item is not consumed in the process.",
-            position = 6,
+            position = 7,
             section = toggles
     )
     default boolean withdrawAll() {
@@ -211,7 +221,7 @@ public interface BanksBankStanderConfig extends Config {
             keyName = "randomSelection",
             name = "randomSelection",
             description = "select random item in inventory?",
-            position = 7,
+            position = 8,
             section = toggles
     )
     default boolean randomSelection() {
