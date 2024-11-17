7de1a2ef20f94e53d6093cb482a3116be5484188
SexyPinata <61428716+SexyPinata@users.noreply.github.com>
2024-11-17 03:42:05 +0100

    - **Rs2Npc**   - *Log message "Natural mouse is not enabled, can't hover" is now only shown when the devDebug flag is enabled.*

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/npc/Rs2Npc.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/npc/Rs2Npc.java
@@ -454,7 +454,8 @@ public class Rs2Npc {
      */
     public static boolean hoverOverActor(Actor actor) {
         if (!Rs2AntibanSettings.naturalMouse) {
-            Microbot.log("Natural mouse is not enabled, can't hover");
+            if(Rs2AntibanSettings.devDebug)
+                Microbot.log("Natural mouse is not enabled, can't hover");
             return false;
         }
         Point point = Rs2UiHelper.getClickingPoint(Rs2UiHelper.getActorClickbox(actor), true);
