8a5dcb5c0cadd17b8442429a4f6b4d957dfd3c26
chsami <sami.chkhachkhi@gmail.com>
2024-11-25 11:02:02 +0100

    add PvpScript

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/qualityoflife/scripts/pvp/PvpScript.java
@@ -0,0 +1,35 @@
+package net.runelite.client.plugins.microbot.qualityoflife.scripts.pvp;
+
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.Script;
+import net.runelite.client.plugins.microbot.qualityoflife.QoLConfig;
+
+import java.util.concurrent.TimeUnit;
+
+public class PvpScript extends Script {
+
+    public boolean run(QoLConfig config) {
+        mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
+            try {
+                if (!Microbot.isLoggedIn()) return;
+                if (!super.run()) return;
+                long startTime = System.currentTimeMillis();
+
+
+                long endTime = System.currentTimeMillis();
+                long totalTime = endTime - startTime;
+                System.out.println("Total time for loop " + totalTime);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                Microbot.log(ex.getMessage());
+            }
+        }, 0, 1000, TimeUnit.MILLISECONDS);
+        return true;
+    }
+
+    @Override
+    public void shutdown() {
+        super.shutdown();
+    }
+}
