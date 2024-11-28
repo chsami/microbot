2eb400c0c0bc597989b2d2cdf527fe9820213a40
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2024-11-27 21:33:03 -0500

    fix: updated worldpoint for tai bwo wannai village patch feat: add civitas illa fortis & trolliehm farm patches

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/walker/enums/FruitTrees.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/walker/enums/FruitTrees.java
@@ -11,7 +11,7 @@ public enum FruitTrees {
     FARMING_GUILD("Farming Guild", new WorldPoint(1243, 3756, 0)),
     GNOME_STRONGHOLD("Gnome Stronghold", new WorldPoint(2473, 3446, 0)),
     TREE_GNOME_VILLAGE("Tree Gnome Village", new WorldPoint(2489, 3182, 0)),
-    TAI_BWO_WANNAI("Tai Bwo Wannai", new WorldPoint(2767, 3215, 0)),
+    TAI_BWO_WANNAI("Tai Bwo Wannai", new WorldPoint(2798, 3101, 0)),
     PRIFDDINAS("Prifddinas", new WorldPoint(3292, 6117, 0));
 
     private final String name;
--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/walker/enums/Herbs.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/walker/enums/Herbs.java
@@ -8,10 +8,12 @@ public enum Herbs {
     NONE("None"),
     ARDOUGNE("Ardougne", new WorldPoint(2668, 3375, 0)),
     CATHERBY("Catherby", new WorldPoint(2811, 3465, 0)),
+    CIVITAS_ILLA_FORTIS("Civitas illa Fortis", new WorldPoint(1583, 3094, 0)),
     FALADOR("Falador", new WorldPoint(3056, 3310, 0)),
     FARMING_GUILD("Farming Guild", new WorldPoint(1238, 3729, 0)),
     KOUREND("Kourend", new WorldPoint(1736, 3553, 0)),
-    MORYTANIA("Morytania", new WorldPoint(3600, 3523, 0));
+    MORYTANIA("Morytania", new WorldPoint(3600, 3523, 0)),
+    TROLLIEHM("Trolliehm", new WorldPoint(2828, 3695, 0));
 
     private final String name;
     private WorldPoint worldPoint;
