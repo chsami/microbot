198f77d270de058ba1f6bf7cffa6c6bee6f0e661
SexyPinata <61428716+SexyPinata@users.noreply.github.com>
2024-12-08 05:30:22 +0100

    ## QoL Plugin - **Updated**: Replaced a deprecated method in the API.

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/qualityoflife/scripts/NeverLogoutScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/qualityoflife/scripts/NeverLogoutScript.java
@@ -2,7 +2,7 @@ package net.runelite.client.plugins.microbot.qualityoflife.scripts;
 
 import net.runelite.api.events.GameTick;
 import net.runelite.client.plugins.microbot.util.keyboard.Rs2Keyboard;
-import net.runelite.client.plugins.microbot.util.math.Random;
+import net.runelite.client.plugins.microbot.util.math.Rs2Random;
 import net.runelite.client.plugins.microbot.util.player.Rs2Player;
 
 import java.awt.event.KeyEvent;
@@ -12,7 +12,7 @@ public class NeverLogoutScript {
 
     public static void onGameTick(GameTick event) {
         if (Rs2Player.checkIdleLogout(randomDelay)) {
-            randomDelay = Random.randomDelay();
+            randomDelay = Rs2Random.between(1000,3000);
             Rs2Keyboard.keyPress(KeyEvent.VK_BACK_SPACE);
         }
     }
