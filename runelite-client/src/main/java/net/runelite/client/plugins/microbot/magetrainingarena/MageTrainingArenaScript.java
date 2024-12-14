7cceb6cc088647af3803bfdfa3ebd7f7985c36b8
chsami <sami.chkhachkhi@gmail.com>
2024-12-14 20:02:03 +0100

    MageTrainingArenaScript graveyard room fix

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/magetrainingarena/MageTrainingArenaScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/magetrainingarena/MageTrainingArenaScript.java
@@ -471,7 +471,7 @@ public class MageTrainingArenaScript extends Script {
         var foodChute = Rs2GameObject.findObjectByLocation(new WorldPoint(3354, 9639, 1));
 
         var boneGoal = 28 - Rs2Inventory.items().stream().filter(x -> x.name.equalsIgnoreCase("Animals' bones")).count();
-        if (mtaPlugin.getGraveyardRoom().getCounter().getCount() >= boneGoal){
+        if (mtaPlugin.getGraveyardRoom().getCounter() != null && mtaPlugin.getGraveyardRoom().getCounter().getCount() >= boneGoal){
             Rs2Magic.cast(btp ? MagicAction.BONES_TO_PEACHES : MagicAction.BONES_TO_BANANAS);
             Rs2Player.waitForAnimation();
             return;
